069b94e164ae12975b327bb9d3121e08d38e1ee4
sergeemmanuelk <serge_em@outlook.fr>
2023-06-02 17:55:44 +0000

    update

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -2,12 +2,20 @@
 <html lang="en">
 <head>
     <meta charset="UTF-8">
-    <meta name="viewport"
-          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta name="viewport" content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Ma super application Laravel</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-9ndCyUaIbzAi2FUVXJi0CjmCapSmO7SnpJef0486qhLnuZ2cdeRhO02iuK6FUUVM" crossorigin="anonymous">
 </head>
 <body>
-    @yield('content')
+    <div class="container">
+        <div class="row justify-content-center">
+            <div class="col-lg-12">
+                @yield('content')
+            </div>
+        </div>
+    </div>
+
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js" integrity="sha384-geWF76RCwLtnZ8qwWowPQNguL3RmwHVBC9FhGdlKrxdiJJigb/j/68SIy3Te4Bkz" crossorigin="anonymous"></script>
 </body>
 </html>
--- resources/views/books/index.blade.php
+++ resources/views/books/index.blade.php
@@ -1,20 +1,43 @@
 @extends('app')
 
 @section('content')
-    @foreach($books as $book)
-        <p>
-            {{ $book->title }} de {{ $book->user->name }}
-        </p>
-        <br>
-        <p>
-            <a href="{{ route('books.edit', $book) }}">Editer</a>
-            <form action="{{ route('books.destroy', $book) }}" method="POST">
-                @csrf
-                @method('DELETE')
-                <button type="submit">Supprimer</button>
-            </form>
-        </p>
-    @endforeach
+    
+    <table class="table">
+        <thead>
+            <tr>
+                <th scope="col">#</th>
+                <th scope="col">Titre</th>
+                <th scope="col">Description</th>
+                <th scope="col">Utilisateur</th>
+                <th scope="col">Date de publication</th>
+                <th scope="col">Ajouté le</th>
+                <th scope="col">Modifié le</th>
+                <th scope="col">Actions</th>
+            </tr>
+        </thead>
+        <tbody>
+            @foreach($books as $book)
+            <tr>
+                <th scope="row">{{ $book->id }}</th>
+                <td>{{ $book->title }}</td>
+                <td>{{ $book->description }}</td>
+                <td>{{ $book->user->name }}</td>
+                <td>{{ $book->published_date }}</td>
+                <td>{{ $book->created_at }}</td>
+                <td>{{ $book->updated_at }}</td>
+                <td>
+                    <a href="{{ route('books.edit', $book) }}">Editer</a>
+                    <form action="{{ route('books.destroy', $book) }}" method="POST">
+                        @csrf
+                        @method('DELETE')
+                        <button type="submit">Supprimer</button>
+                    </form>
+                </td>
+            </tr>
+            @endforeach
+        </tbody>
+    </table>
+    
 @endsection
 
 
